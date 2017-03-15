62113c9def4d52e2ed1c08c17453c5e8818ef29f
Xciles <xciles@gmail.com>
2017-03-15 16:46:56 +0100

    Adding base classes for websockets

---
+++ NetCoreLedger/NetCoreLedger/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreLedger.Sockets;
+
+namespace NetCoreLedger.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder MapWebSocketManager(this IApplicationBuilder builder, PathString path, WebSocketHandler handler)
+        {
+            return builder.Map(path, (app) => app.UseMiddleware<WebSocketMiddleware>(handler));
+        }
+
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddTransient<WebSocketConnectionManager>();
+
+            foreach (var type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if (type.GetTypeInfo().BaseType == typeof(WebSocketHandler))
+                {
+                    services.AddSingleton(type);
+                }
+            }
+
+            return services;
+        }
+    }
+}
---
+++ NetCoreLedger/NetCoreLedger/Middleware/WebSocketMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using NetCoreLedger.Sockets;
+
+namespace NetCoreLedger.Middleware
+{
+    public class WebSocketMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly WebSocketHandler _webSocketHandler;
+
+        public WebSocketMiddleware(RequestDelegate next, WebSocketHandler webSocketHandler)
+        {
+            _next = next;
+            _webSocketHandler = webSocketHandler;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                // Not a web socket request
+                await _next.Invoke(context);
+                return;
+            }
+
+            var socket = await context.WebSockets.AcceptWebSocketAsync();
+            await _webSocketHandler.OnConnected(socket);
+
+            await Receive(socket, async (result, buffer) =>
+            {
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    await _webSocketHandler.ReceiveAsync(socket, result, buffer);
+                }
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await _webSocketHandler.OnDisconnected(socket);
+                }
+            });
+
+            //TODO - investigate the Kestrel exception thrown when this is the last middleware
+            await _next.Invoke(context);
+        }
+
+        private async Task Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                handleMessage(result, buffer);
+            }
+        }
+    }
+}
